6376c128018cbdc20e62c82b329060f117680d4a
phantamanta44 <evandalong@gmail.com>
2019-05-21 22:51:25 -0500

    Include coremod attrs in jar manifest

--- src/main/java/io/github/phantamanta44/tmemes/MEMES.java
+++ src/main/java/io/github/phantamanta44/tmemes/MEMES.java
@@ -13,7 +13,7 @@ public class MEMES {
 
     public static final String MOD_ID = "tmemes";
     public static final String NAME = "MEMES";
-    public static final String VERSION = "1.0.1";
+    public static final String VERSION = "1.0.2";
     public static final String MOD_PREF = MOD_ID + ":";
 
     @SuppressWarnings("NullableProblems")
