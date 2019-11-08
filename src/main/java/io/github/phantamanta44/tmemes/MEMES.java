1ee363bb288170eec67275940b85d0a5d06864b4
phantamanta44 <evandalong@gmail.com>
2019-11-08 17:40:09 -0600

    Bump up to 1.0.8, bump deps up to mantle 1.3.3.56, tcon 2.13.0.174, mek 9.8.2.389, conarm 1.2.5.3

--- src/main/java/io/github/phantamanta44/tmemes/MEMES.java
+++ src/main/java/io/github/phantamanta44/tmemes/MEMES.java
@@ -14,7 +14,7 @@ public class MEMES {
 
     public static final String MOD_ID = "tmemes";
     public static final String NAME = "MEMES";
-    public static final String VERSION = "1.0.7";
+    public static final String VERSION = "1.0.8";
     public static final String MOD_PREF = MOD_ID + ":";
 
     @SuppressWarnings("NullableProblems")
